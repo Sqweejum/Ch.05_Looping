f0f4611743561acd4dd2658d125ee9933af5253d
cobbzac <cobbzac@urbandaleschools.com>
2019-10-31 13:35:23 -0500

    finished 5.0

--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -6,9 +6,3 @@ COIN TOSS PROGRAM
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
-print("This program takes three numbers and returns the sum.")
-total = 0
-for i in range(3):
-    x = input("Enter a number: ")
-    total = total + x
-print("The total is:", total)
